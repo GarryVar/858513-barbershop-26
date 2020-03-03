5066195ccd251b76f55aa241b8657c8619dafea8
GarryVar <darisvet.sokolov@yandex.ru>
2020-03-03 07:32:06 +0300

    Добавляет файлы для проекта

---
+++ source/js/slider.js
@@ -0,0 +1,43 @@
+(() => {
+  let leftNumber = 0;
+
+  function findSlider() {
+    let slider = document.querySelector('.gallery__list');
+
+      return slider;
+  }
+
+
+  function backMove(slide) {
+    if (leftNumber != 0) {
+        leftNumber = leftNumber + 100;
+        slide.style.left = `${leftNumber}%`;
+
+    } else leftNumber = false;
+  }
+
+
+  function forwardMove(slide) {
+    if (leftNumber > -200) {
+        leftNumber = leftNumber - 100;
+        slide.style.left = `${leftNumber}%`;
+
+      } else leftNumber = -200;
+  }
+
+
+  window.addEventListener('click', e => {
+    let elementTarget = e.target;
+
+    if (elementTarget.dataset.action === 'forward') {
+        e.preventDefault();
+        forwardMove(findSlider());
+
+    } else if (elementTarget.dataset.action === 'back') {
+        e.preventDefault();
+        backMove(findSlider());
+    }
+
+  });
+})();
+
